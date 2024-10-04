26a2c8258d82ac1d95d61428ec25ebcd00ffa8ef
Imamasharmeen <imamasharmeen@gmail.com>
2024-10-04 11:04:59 +0300

    added ternary operator

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const loadAllPost = async() => {
-    const response = await fetch(' https://openapi.programming-hero.com/api/retro-forum/posts')
+const loadAllPosts = async(category) => {
+   // console.log(`https://openapi.programming-hero.com/api/retro-forum/posts${category?`?category = ${category}` : ''}`)
+
+    const response = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts${category?`?category=${category}`:''}`)
     const data = await response.json();
     console.log(data.posts)
 }
-loadAllPost()
+loadAllPosts()
+
+const handleSearchByCategory = () =>{
+    const searchText = document.getElementById('searchPosts').value;
+    loadAllPosts(searchText)
+}
